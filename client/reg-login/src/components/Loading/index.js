1d4f72fbb600340fa9cdf6ba404e00ca74a89943
Denis Didenko <didenkodenis1404@gmail.com>
2022-06-29 11:42:28 +0300

    tabs2

--- client/reg-login/src/components/Loading/index.js
+++ client/reg-login/src/components/Loading/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './loading.css';
 
 function Loading() {
-	return (
-		<div className='loading-container'>
-			<div className='loading'>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='loading-container'>
+      <div className='loading'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
--- client/reg-login/src/components/LoginForm/index.js
+++ client/reg-login/src/components/LoginForm/index.js
@@ -1,61 +1,62 @@
 import { useContext, useRef, useState } from 'react';
 import AuthContext from '../../context/auth';
+
 import FormItem from '../FormItem';
 
 const LoginForm = () => {
-	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-	});
-	const [errors, setErrors] = useState({
-		email: '',
-		password: '',
-	});
-	const formRef = useRef();
-	const { auth } = useContext(AuthContext);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  });
+  const formRef = useRef();
+  const { auth } = useContext(AuthContext);
 
-	const inputChangeHandler = e => {
-		setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }));
-	};
+  const inputChangeHandler = e => {
+    setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-	const formSubmitHandler = async e => {
-		e.preventDefault();
+  const formSubmitHandler = async e => {
+    e.preventDefault();
 
-		const response = await auth.login(formData.email, formData.password);
-		if (response?.data?.error) {
-			validateForm(response.data.error);
-		} else {
-			resetForm();
-		}
-	};
+    const response = await auth.login(formData.email, formData.password);
+    if (response?.data?.error) {
+      validateForm(response.data.error);
+    } else {
+      resetForm();
+    }
+  };
 
-	const validateForm = responseErrors => {
-		Object.keys(errors).forEach(err => {
-			if (responseErrors.name === err) {
-				setErrors(prev => ({ ...prev, [responseErrors.name]: responseErrors.message }));
-			} else {
-				setErrors(prev => ({ ...prev, [err]: '' }));
-			}
-		});
-	};
+  const validateForm = responseErrors => {
+    Object.keys(errors).forEach(err => {
+      if (responseErrors.name === err) {
+        setErrors(prev => ({ ...prev, [responseErrors.name]: responseErrors.message }));
+      } else {
+        setErrors(prev => ({ ...prev, [err]: '' }));
+      }
+    });
+  };
 
-	const resetForm = () => {
-		setFormData({ email: '', password: '' });
-		formRef.current.reset();
-	};
+  const resetForm = () => {
+    setFormData({ email: '', password: '' });
+    formRef.current.reset();
+  };
 
-	return (
-		<form action='/login' ref={formRef} onSubmit={formSubmitHandler}>
-			<h2 className='form-title'>Log in</h2>
+  return (
+    <form action='/login' ref={formRef} onSubmit={formSubmitHandler}>
+      <h2 className='form-title'>Log in</h2>
 
-			<FormItem name='email' inputType='email' label='Email:' error={errors.email} onInputChange={inputChangeHandler} />
-			<FormItem name='password' inputType='password' label='Password:' error={errors.password} onInputChange={inputChangeHandler} />
+      <FormItem name='email' inputType='email' label='Email:' error={errors.email} onInputChange={inputChangeHandler} />
+      <FormItem name='password' inputType='password' label='Password:' error={errors.password} onInputChange={inputChangeHandler} />
 
-			<div className='form-submit'>
-				<button type='submit'>Submit</button>
-			</div>
-		</form>
-	);
+      <div className='form-submit'>
+        <button type='submit'>Submit</button>
+      </div>
+    </form>
+  );
 };
 
 export default LoginForm;
